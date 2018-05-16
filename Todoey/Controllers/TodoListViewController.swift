11ba2a929c170dfb88ecbbf3896a92c633062c9f
JBM <bumkuns@gmail.com>
2018-05-17 00:29:28 +0900

    더미 리스트에 내용추가

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -18,48 +18,48 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
         
         let newItem = Item()
-        newItem.title = "Find Mike"
+        newItem.title = "게임하지 않기"
         itemArray.append(newItem)
         
         let newItem2 = Item()
-        newItem2.title = "Find Mike"
+        newItem2.title = "만화보지 않기"
         itemArray.append(newItem2)
         
         let newItem3 = Item()
-        newItem3.title = "Find Mike"
+        newItem3.title = "사업하는 척하지 않기"
         itemArray.append(newItem3)
         
         let newItem4 = Item()
-        newItem4.title = "Find Mike"
+        newItem4.title = "늦잠자지 않기"
         itemArray.append(newItem4)
         
         let newItem7 = Item()
-        newItem7.title = "Find Mike"
+        newItem7.title = "늦게자지 않기"
         itemArray.append(newItem7)
         
         let newItem5 = Item()
-        newItem5.title = "Find Mike"
+        newItem5.title = "오늘 하루 한일 일기쓰기"
         itemArray.append(newItem5)
         
         let newItem6 = Item()
-        newItem6.title = "Find Mike"
+        newItem6.title = "내일 할일 떠올리기"
         itemArray.append(newItem6)
         
         
         let newItem8 = Item()
-        newItem8.title = "Find Mike"
+        newItem8.title = "탈각본을 하게된 후를 상상하기"
         itemArray.append(newItem8)
         
         let newItem9 = Item()
-        newItem9.title = "Find Mike"
+        newItem9.title = "돈과 시간의 여유가 넘치고 남으면 뭘 할지 생각하기"
         itemArray.append(newItem9)
         
         
         
         
-        //if let items = defaults.array(forKey: "ItemArrayList") as? [String]{
-      //      itemArray = items
-      //  }
+        //if let items = defaults.object(forKey: "ItemArrayList") as? [Item]{
+       //     itemArray = items
+        //}
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -74,14 +74,18 @@ class TodoListViewController: UITableViewController {
         
        // let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoitemCell", for: indexPath)
         
-        let item = itemArray[indexPath.row].title
-        cell.textLabel?.text = itemArray[indexPath.row].title
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
         
-        if itemArray[indexPath.row].done == true {
-            cell.accessoryType = .checkmark
-        } else {
-            cell.accessoryType = .none
-        } // 번호에 맞는 어레이의 변수를 확인하고 완료가 체크되어있다면 체크마크된 셀을 내보냄, 테이블뷰에서 하는것과 다른점은 테이블 뷰를 뿌리고 체크마크를 붙이는가, 아니면 셀을 내보낼때 체크마크를 붙여서 내보내는가.
+        // Value to change = Condition to Check A==B ? valueIfTrue : valueIfFalse
+        cell.accessoryType = item.done == true ? .checkmark : .none
+        
+        //
+        //if item.done == true {
+         //   cell.accessoryType = .checkmark
+        //} else {
+          //  cell.accessoryType = .none
+        //} // 번호에 맞는 어레이의 변수를 확인하고 완료가 체크되어있다면 체크마크된 셀을 내보냄, 테이블뷰에서 하는것과 다른점은 테이블 뷰를 뿌리고 체크마크를 붙이는가, 아니면 셀을 내보낼때 체크마크를 붙여서 내보내는가.
         
         
         return cell // 셀을 하나 잡아서 순서대로, 아이템어레이의 순서대로 추가
@@ -90,7 +94,7 @@ class TodoListViewController: UITableViewController {
     }
     //Mark - Tableview Delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
+        //print(item)
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         //if itemArray[indexPath.row].done == true {
           //  itemArray[indexPath.row].done = false
